10a70eeb2a4940743f338d053e359f0b4e9cffc9
lorenzfedl-cmyk <lorenz.fedl@gmail.com>
2026-03-08 10:13:53 +0100

    Test Daten einlesen und COV-Matrix erstellen

---
+++ scripts/main_script.R
@@ -0,0 +1,17 @@
+# libraries laden
+library(tidyquant)
+
+# daten laden
+meta_test_2025 <- read.csv2("data/DAX_metadata_2025.csv", stringsAsFactors = FALSE)
+returns_test_2025 <- read.csv2("data/DAX_returns_2025.csv", stringsAsFactors = FALSE)
+
+returns_numeric <- returns_test_2025[, -1]
+
+# 0 als fehlende Werte interpretieren
+returns_numeric[returns_numeric == 0] <- NA
+
+# Kovarianzmatrix berechnen
+cov_matrix <- cov(returns_numeric, use = "pairwise.complete.obs")
+
+
+
