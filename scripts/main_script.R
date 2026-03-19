0f81bb30534447b7ab6a29e93d9ee981d167dc6f
lorenzfedl-cmyk <lorenz.fedl@gmail.com>
2026-03-19 22:08:59 +0100

    Listen

--- scripts/main_script.R
+++ scripts/main_script.R
@@ -3,7 +3,6 @@ library(tidyquant)
 library(readxl)
 
 # daten laden
-
 meta_2010 <- read_excel("data/FTSE 100 FROM 2010 TO 2025.xlsx", sheet = "2010 META")
 meta_2015 <- read_excel("data/FTSE 100 FROM 2010 TO 2025.xlsx", sheet = "2015 META")
 meta_2020 <- read_excel("data/FTSE 100 FROM 2010 TO 2025.xlsx", sheet = "2020 META")
@@ -12,6 +11,41 @@ return_2010 <- read_excel("data/FTSE 100 FROM 2010 TO 2025.xlsx", sheet = "2010 
 return_2015 <- read_excel("data/FTSE 100 FROM 2010 TO 2025.xlsx", sheet = "2015 RETURN")
 return_2020 <- read_excel("data/FTSE 100 FROM 2010 TO 2025.xlsx", sheet = "2020 RETURN")
 return_2025 <- read_excel("data/FTSE 100 FROM 2010 TO 2025.xlsx", sheet = "2025 RETURN")
+
+# Listen erstellen für Schleifen
+meta_list <- list(
+  meta_2010,
+  meta_2015,
+  meta_2020,
+  meta_2025
+)
+return_list <- list(
+  return_2010,
+  return_2015,
+  return_2020,
+  return_2025
+)
+
+
+
+# Datumspalte entfernen
+return_2010 <- return_2010[, -1]
+return_2015 <- return_2015[, -1]
+return_2020 <- return_2020[, -1]
+return_2025 <- return_2025[, -1]
+
+# 0 als fehlende Werte interpretieren (0 wird zu NA)
+return_2010[return_2010 == 0] <- NA
+return_2015[return_2015 == 0] <- NA
+return_2020[return_2020 == 0] <- NA
+return_2025[return_2025 == 0] <- NA
+
+# Kovarianzmatrizen nur mit "kompletten/vorhandenen" Zahlenpaare berechnen
+a3 <- cov(a2, use = "pairwise.complete.obs")
+b3 <- cov(b2, use = "pairwise.complete.obs")
+c3 <- cov(c2, use = "pairwise.complete.obs")
+d3 <- cov(d2, use = "pairwise.complete.obs")
+
 
 
 meta_test_2025 <- read.csv2("data/2025_metadata_test.csv", stringsAsFactors = FALSE)
